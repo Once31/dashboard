798c8ab5b2a52231fc108039b82333f24a465686
once31 <ajaychavan9096@gmail.com>
2024-03-06 01:06:24 +0530

    vercel api deployment

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, {Express, Request, Response } from "express";
 import cors from "cors"
 import helmet from "helmet"
+import { config } from "dotenv";
+
+import dashboard from "./routes/dashboard/index.js"
+import mongoose from "mongoose";
+
+
+config();
 
 const app: Express = express();
 
@@ -11,8 +18,29 @@ app.use(cors({
 
 app.use(helmet());
 
+
+
+
+
+
 app.get('/', (_req: Request, resp: Response) => {
     resp.send("Hello World")
 })
+
+app.use("/dashboard", dashboard );
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env["DATABASE_URL"] || "mongodb://localhost:27017/dashboard").then(() => {
+            console.log("Connection with mongodb")
+        }).catch(error => console.log("Mongo Error: ", error));
+    } catch(error) {
+        console.error("error", error)
+    }
+}
+
+connectDB();
+
+
 
 export default app;
